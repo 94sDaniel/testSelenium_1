ec2bbed7f331011fd56e9c4bdd4805df9be73928
94sdaniel <31906371+94sdaniel@users.noreply.github.com>
2023-05-08 12:52:55 -0500

    ultimo repo con moidificaciones finales de cada punto y adicion de tiempo de espera donde requeria espera carga pagina

--- src/main/java/sdaniel/mac/testselenium/TestSelenium.java
+++ src/main/java/sdaniel/mac/testselenium/TestSelenium.java
@@ -58,6 +58,8 @@ public class TestSelenium {
         WebElement checkboxTesting = driver.findElement(By.cssSelector("label[data-filter-value='Testing']"));
         checkboxTesting.click();
 
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
         // hacer clic en el checkbox Female threads
         var foo = wait.until(driver -> {
             return driver.findElement(By.cssSelector("label[data-filter-value='Female threads']"));
@@ -67,61 +69,71 @@ public class TestSelenium {
 
         WebElement viewMore = driver.findElement(By.cssSelector("#CartButton_036124a315644c4290a1c8a03b501fdd"));
 
-        //2. Filtrar por Use Case Testing
-        WebElement testingCheckBox = driver.findElement(By.xpath("//label[@for='cb-filter-testing']"));
-        testingCheckBox.click();
-        
-        //3. Filtrar por Female threads
-        WebElement femaleThreadsCheckBox = driver.findElement(By.xpath("//label[@for='cb-filter-female-threads']"));
-        femaleThreadsCheckBox.click();
-        
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
         //4. Dar click en el botón "View more" del producto WEH Connector TW17V
-        WebElement viewMoreButton = driver.findElement(By.xpath("//a[contains(text(),'WEH® Connector TW17V for testing of components with female thread, pneumatic actuation via valve button, vacuum up to max. 350 bar - Product family')]/../following-sibling::div/a"));
+        WebElement viewMoreButton = driver.findElement(By.xpath("//img[@src ='https://www.weh.com/Admin/Public/GetImage.ashx?width=300&height=300&crop=5&Compression=75&FillCanvas=true&DoNotUpscale=true&image=%2fFiles%2fImages%2f10-Product-Images%2fQuick-Connectors%2fTW17%2fTW17V_ADT_C1-14835_r_web.jpg]"));
         viewMoreButton.click();
-        
+
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
         //5. Seleccionar los valores de los campos "Connects to" y "Operating Pressure PS"
-        WebElement connectsToDropDown = driver.findElement(By.id("//select[@for='VariantSelector_VARGRP62']"));
+        WebElement connectsToDropDown = driver.findElement(By.id("VariantSelector_VARGRP62"));
         Select dropDown = new Select(connectsToDropDown);
         dropDown.selectByVisibleText("M10X1.0 Female Thread");
 
-        WebElement operatingPressurePSDropDown = driver.findElement(By.id("//select[@for='VariantSelector_VARGRP65']"));        Select dropPDown = new Select(connectsToDropDown);
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
+        WebElement operatingPressurePSDropDown = driver.findElement(By.id("VariantSelector_VARGRP65"));
+        Select dropPDown = new Select(connectsToDropDown);
         dropPDown.selectByVisibleText("50 bar");
-        
+
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
         //6. Dar click en el botón Request Free Quote
-        WebElement requestFreeQuoteButton = driver.findElement(By.xpath("//input[@value='Request Free Quote']"));
+        WebElement requestFreeQuoteButton = driver.findElement(By.xpath("//*[@href='Request Free Quote']"));
         requestFreeQuoteButton.click();
-        
+
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
         //7. Diligenciar los campos obligatorios del formulario
-        WebElement firstNameField = driver.findElement(By.xpath("//input[@id='Firstname']"));
+        WebElement firstNameField = driver.findElement(By.id("FirstName"));
         firstNameField.sendKeys("Daniel");
         
-        WebElement lastNameField = driver.findElement(By.xpath("//input[@id='Lastname']"));
+        WebElement lastNameField = driver.findElement(By.id("LastName"));
         lastNameField.sendKeys("Sierra");
         
-        WebElement companyField = driver.findElement(By.xpath("//input[@id='Company']"));
+        WebElement companyField = driver.findElement(By.id("Company"));
         companyField.sendKeys("Davivienda");  
 
-        WebElement addressField = driver.findElement(By.xpath("//input[@id='Address']"));
+        WebElement addressField = driver.findElement(By.id("Address"));
         addressField.sendKeys("Cra 59a 135 60");  
 
-        WebElement cityField = driver.findElement(By.xpath("//input[@id='City']"));
+        WebElement cityField = driver.findElement(By.id("City"));
         cityField.sendKeys("johndoe@example.com");        
 
-        WebElement zipField = driver.findElement(By.xpath("//input[@id='Zip']"));
+        WebElement zipField = driver.findElement(By.id("Zip"));
         zipField.sendKeys("111011");
         
-        WebElement countryField = driver.findElement(By.xpath("//input[@id='Bogota']"));
+        WebElement countryField = driver.findElement(By.id("Country"));
         countryField.sendKeys("Bogota");
         
-        WebElement emailField = driver.findElement(By.xpath("//input[@id='Email']"));
+        WebElement emailField = driver.findElement(By.id("Email"));
         emailField.sendKeys("daniel.sierra@davivienda.com");
         
-        WebElement messageField = driver.findElement(By.xpath("//textarea[@id='message']"));
+        WebElement messageField = driver.findElement(By.id("Comments"));
         messageField.sendKeys("Please send me a quote for the WEH Connector TW17V");
         
         //8. Seleccionar el check que permite obtener la autorización del cliente
-        WebElement authorizationCheckBox = driver.findElement(By.xpath("//input[@id='chk-auth']"));
+        WebElement authorizationCheckBox = driver.findElement(By.cssSelector("label[data-filter-value='ConsentGDPR']"));
         authorizationCheckBox.click();
+
+        //9. Click Send
+        WebElement sendCheck = driver.findElement(By.id("Send"));
+        sendCheck.click();
+
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
     }
     
     
